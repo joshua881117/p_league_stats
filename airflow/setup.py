a34936626a7502c620860da431dfbdfc9527d5b2
林書亞 <joshua881117@gmail.com>
2023-09-28 16:01:18 +0800

    [Add] add airflow file

---
+++ airflow/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from os import path
+from io import open
+
+setup(
+    name='dataflow',  # Required
+    version='1.0.0',  # Required
+    description='dataflow',  # Optional
+)
